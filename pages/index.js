b99b1c71840b33ffaf7a284dee7a39dbcfa443d6
Moon-YJ <tunjeong@gmail.com>
2024-01-22 15:56:53 +0900

    _app.js에 공통 fetching URL 등록

--- pages/index.js
+++ pages/index.js
@@ -19,9 +19,7 @@ export default function Home(props) {
 // ssg 방식으로 페이지 랜더링
 export async function getStaticProps() {
 	// serialize 에러시 반환받은 promise 객체에서 실제 data값을 비구조화할당으로 받아서 props로 전달
-	const { data } = await axios.get(
-		'https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood'
-	);
+	const { data } = await axios.get('/filter.php?c=Seafood');
 	//console.log(data);
 	// return으로 props객체를 컴포넌트로 전달시 revalidate 속성 없으면 SSG 방식, 해당 속성 추가하면 ISR방식(현재는 24시간마다 새로운 data fetching하게 처리)
 	return { props: data, revalidate: 60 * 60 * 24 };
