737d89c5b3fe9e4d79a1cea3b6e6b1496c83a33c
Moon-YJ <tunjeong@gmail.com>
2024-01-23 17:58:44 +0900

    Category 컴포넌트 작업(Text컴포넌트에 onClick props로 값 전달)

--- components/atoms/text/Text.jsx
+++ components/atoms/text/Text.jsx
@@ -19,6 +19,8 @@ const orbitron = Orbitron({
 	variable: '--font-orbitron',
 });
 
+// Text 컴포넌트에서 부모로부터 이벤트핸들러 받을때, 보통 전달받는 값은 순서, fetching data, e 객체
+// 위의 값 세가지를 비구조할당으로 전달 받음(없으면 아무런 변화 X, 있으면 활용)
 export default function Text({
 	tagName = 'p',
 	children,
@@ -26,6 +28,9 @@ export default function Text({
 	styleType,
 	className,
 	isOn = false,
+	onClick,
+	data,
+	idx,
 }) {
 	// JSX를 반환하는 리액트 내장 메서드
 	// React.createElement(elementType: string, props: object, children: JSX Node)
@@ -42,6 +47,9 @@ export default function Text({
 				className,
 				isOn && styles.on
 			),
+			// onClick props로 전달되는 핸들러함수에 어떤 값이 쓰일지 모르기 때문에 일단 다 연결
+			// 주의: 객체로 묶어주지 않으면 특정 인수가 전달되지 않았을때 파라미터 순서가 변경되므로 이를 방지하기 위해 객체로 인수를 묶음
+			onClick: (e) => onClick({ e, idx, data }),
 		},
 		// props로 전달받은 url값이 있으면 React.createElement로 Link 컴포넌트로 props값 설정한 뒤, children 감싸서 리턴
 		url ? React.createElement(Link, { href: url }, children) : children
--- pages/gallery/index.jsx
+++ pages/gallery/index.jsx
@@ -2,14 +2,31 @@ import clsx from 'clsx';
 import styles from './gallery.module.scss';
 import { useState } from 'react';
 import SearchBar from '@/components/molecules/searchBar/SearchBar';
+import Category from '@/components/molecules/category/Category';
 
 export default function Gallery() {
 	const [Val, setVal] = useState('');
+	const fetchData = [
+		{ name: 'David', age: 20 },
+		{ name: 'Amy', age: 30 },
+		{ name: 'Paul', age: 10 },
+	];
+	const names = fetchData.map((data) => data.name);
+	const handleClick = ({ e, idx, data }) => {
+		console.log(e.target.innerText);
+		console.log(idx);
+		console.log(data[idx]);
+	};
 
 	return (
 		<section className={clsx(styles.gallery)}>
 			<h1>Gallery page</h1>
 			<SearchBar value={Val} onChange={setVal} />
+			<Category
+				fetchDataArr={fetchData}
+				onClick={handleClick}
+				nameArr={names}
+			/>
 		</section>
 	);
 }
